9932a408cc198575b16bcf71a6c692d268be0880
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2022-02-01 00:56:12 -0500

    start creating necessary routes

--- Develop/server.js
+++ Develop/server.js
@@ -3,13 +3,34 @@ const path = require('path');
 
 const app = express();
 
+const getApiNotes = (req, res) => {
+    const dbFilepath = path.join(__dirname, 'db', 'db.json');
+    res.sendFile(dbFilepath);
+}
+
+const getCSS = (req, res) => {
+    const cssFilepath = path.join(__dirname, 'public', 'assets', 'css', 'styles.css');
+    res.setHeader('content-type', 'text/css');
+    res.sendFile(cssFilepath);
+}
+
+const getJS = (req, res) => {
+    const jsFilepath = path.join(__dirname, 'public', 'assets', 'js', 'index.js');
+    res.setHeader('content-type', 'text/javascript');
+    res.sendFile(jsFilepath);
+}
+
 const getIndex = (req, res) => {
     const indexFilepath = path.join(__dirname, 'public', 'index.html');
     res.setHeader('content-type', 'text/html');
     res.sendFile(indexFilepath);
 }
 
+
 app.get('/', getIndex);
+app.get('/api/notes', getApiNotes);
+app.get('/assets/css/style.css', getCSS);
+app.get('/assets/js/index.js', getJS);
 
 app.listen(3001, () => {
     console.log('API server now on port 3001!');
